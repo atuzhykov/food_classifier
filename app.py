9899a60de46d34b998a2afb62ecbb0cc2c3a0833
Andrii Tuzhykov <andrew@unrealists.com>
2019-12-24 22:41:22 +0200

    added meta clf

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import collections
 import numpy as np
 import math
 from food_ml import pipeline
+import os
 
 
 def food_index(food_data):
@@ -29,7 +30,23 @@ def food_index(food_data):
     return label
 
 def food_label_classifier(food_data:dict, algo = 'rf'):
+    food_name = food_data.name
+    food_data = food_data.totals
     labels = ["green","yellow","red"]
+
+
+
+    # in case some features are missing we restore them
+    features = ['protein','fat','carbohydrates','sugar','sodium','calories']
+    for feature in features:
+        if feature not in food_data:
+            client = myfitnesspal.Client(username=os.environ.get('email'), password=os.environ.get('password'))
+            food_item = client.get_food_search_results(food_name)[0]
+            meal = client.get_food_item_details(food_item.mfp_id)
+            food_data[feature] = getattr(meal, feature)
+
+
+
     X = np.array([food_data['protein'],food_data['fat'],food_data['carbohydrates'],food_data['sugar'],food_data['sodium'],food_data['calories']]).reshape(1, -1)
 
     if algo == 'lr':
@@ -53,7 +70,7 @@ def meta_classifier(food_data):
     decisions = []
     for algo in algos:
         decisions.append(food_label_classifier(food_data,algo=algo))
-    decisions.append(food_index(food_data))
+    decisions.append(food_index(food_data.totals))
     return collections.Counter(decisions).most_common(1)[0][0]
     
 
@@ -62,31 +79,40 @@ def meta_classifier(food_data):
      
 
 
+def last_day_food_extractor(client):
+    day_food_data = [ ]
+    day = client.get_date(datetime.datetime.now().year, datetime.datetime.now().month, datetime.datetime.now().day)
+    for meal in day.meals:
+        for entry in meal:
+            food_data = dict()
+            food_data['name'] = entry.name
+            food_data['calories'] = entry.totals['calories']
+            food_data['label'] = meta_classifier(entry)
+            day_food_data.append(food_data)
+
+    return day_food_data
+
+
+
 app = Flask(__name__)
 api = Api(app)
 
 parser = reqparse.RequestParser()
-parser.add_argument('email')
-parser.add_argument('password')
+parser.add_argument('email', type=str)
+parser.add_argument('password', type=str)
 
 
 class FoodClassifier(Resource):
     def post(self):
         args = parser.parse_args()
-        email = args['email']
-        password = args['password']
+        email = args['email'].strip()
+        password = args['password'].strip()
+        os.environ['email'] = email
+        os.environ['password'] = password
         client = myfitnesspal.Client(username=email, password=password)
-        day_food_data = [ ]
-        day = client.get_date(datetime.datetime.now().year, datetime.datetime.now().month, datetime.datetime.now().day)
-        for meal in day.meals:
-            for entry in meal:
-                food_data = dict()
-                food_data['name'] = entry.name
-                food_data['calories'] = entry.totals['calories']
-                food_data['label'] = meta_classifier(entry.totals)
-                day_food_data.append(food_data)
+        result = last_day_food_extractor(client)
         
-        return jsonify(day_food_data)
+        return jsonify(result)
 
 api.add_resource(FoodClassifier, '/foodclassifier') 
 
