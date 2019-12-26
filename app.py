314ea14190301634a63202805d21afea8c1231b3
Andrii Tuzhykov <andrew@unrealists.com>
2019-12-26 14:52:18 +0200

    added KNN imputer

--- app.py
+++ app.py
@@ -8,47 +8,57 @@ import numpy as np
 import math
 from food_ml import pipeline
 import os
+from sklearn.impute import KNNImputer
+import pandas as pd
+
+# Only for HEROKU deployment due to daily erasing all filesystem
+url = 'https://raw.githubusercontent.com/atuzhykov/food_classifier/master/MFP_scrapped_food_without_names.csv'
+
+import os.path
+if not os.path.isfile('GradientBoostingRegressor.pkl'):
+    pipeline(url)
 
 
-def food_index(food_data):
-    protein = food_data['protein']
-    fats = food_data['fat']
-    carbs = food_data['carbohydrates']
-    sugar = food_data['sugar']
-    calories = food_data['calories']
-    sodium = food_data['sodium']
-    w =[0.4, 0.2, 0.1 ]
-    index =  w[0]*carbs + w[1]*fats + w[2]*protein + abs(calories*0.1 - sugar) + math.exp(0.0001*sodium)
-    
-    label = {
-               index < 9: 'green',
-        9 <= index < 25:   'yellow',
-          25 <= index:  'red',
-     
-    }[True]
 
-    return label
 
-def food_label_classifier(food_data:dict, algo = 'rf'):
-    food_name = food_data.name
-    food_data = food_data.totals
+def KNN_imputer(food_data,missed_features):
+    features = ['protein','fat','carbohydrates','sugar','sodium','calories']
+  
+   
+    Y = pd.read_csv(url).drop('class',1).to_numpy()
+    nan = np.nan
+    protein = nan if 'protein' not in food_data else food_data['protein']
+    fat =  nan if 'fat' not in food_data else food_data['fat']
+    carbohydrates =  nan if 'carbohydrates' not in food_data else food_data['carbohydrates']
+    sugar = nan if 'sugar' not in food_data else  food_data['sugar']
+    sodium =  nan if 'sodium' not in food_data else food_data['sodium']
+    calories =  nan if 'calories' not in food_data else food_data['calories']
+    print('Vector before restoring {}'.format(np.array([[protein,fat,carbohydrates,sugar,sodium,calories]])))
+            
+
+    Y = np.concatenate((Y, np.array([[protein,fat,carbohydrates,sugar,sodium,calories]])))
+    imputer = KNNImputer(n_neighbors=2, weights="uniform")
+    X = imputer.fit_transform(Y)[-1].reshape(1, -1)
+    print('Restored via KNNImputer vector {}'.format(X))
+    return X
+
+
+def food_label_classifier(X, algo = 'rf'):
+
     labels = ["green","yellow","red"]
 
+    if algo == 'formula':
+        w =[0.4, 0.2, 0.1 ]
+        index =  w[0]*X[0,2] + w[1]*X[0,1] + w[2]*X[0,0] + abs(X[0,5]*0.1 - X[0,3]) + math.exp(0.0001*X[0,4])
+    
+        label = {
+               index < 9: 'green',
+            9 <= index < 25:   'yellow',
+            25 <= index:  'red',
+     
+        }[True]
 
-
-    # in case some features are missing we restore them
-    features = ['protein','fat','carbohydrates','sugar','sodium','calories']
-    for feature in features:
-        if feature not in food_data:
-            food_data[feature] = 0
-            # client = myfitnesspal.Client(username=os.environ.get('email'), password=os.environ.get('password'))
-            # food_item = client.get_food_search_results(food_name)[0]
-            # meal = client.get_food_item_details(food_item.mfp_id)
-            # food_data[feature] = getattr(meal, feature)
-
-
-
-    X = np.array([food_data['protein'],food_data['fat'],food_data['carbohydrates'],food_data['sugar'],food_data['sodium'],food_data['calories']]).reshape(1, -1)
+        return label
 
     if algo == 'lr':
         classifier = joblib.load('LogisticRegression.pkl')
@@ -67,11 +77,26 @@ def food_label_classifier(food_data:dict, algo = 'rf'):
         return labels[classifier.predict(X).tolist()[0]]
 
 def meta_classifier(food_data):
-    algos = ['rf','ab','lr','gb']
+    food_name = food_data.name
+    food_data = food_data.totals
+    features = ['protein','fat','carbohydrates','sugar','sodium','calories']
+    missed_features = []
+    for feature in features:
+        if feature not in food_data:
+            missed_features.append(feature)
+    
+    if missed_features:
+        print('following features are missing for {}: {}'.format(food_name,' '.join(missed_features)))
+        X = KNN_imputer(food_data, missed_features).reshape(1, -1)
+
+    else:
+        X = np.array([food_data['protein'],food_data['fat'],food_data['carbohydrates'],food_data['sugar'],food_data['sodium'],food_data['calories']]).reshape(1, -1)
+    
+    algos = ['rf','ab','lr','gb','formula']
     decisions = []
     for algo in algos:
-        decisions.append(food_label_classifier(food_data,algo=algo))
-    decisions.append(food_index(food_data.totals))
+        decisions.append(food_label_classifier(X,algo=algo))
+    
     return collections.Counter(decisions).most_common(1)[0][0]
     
 
@@ -108,8 +133,8 @@ class FoodClassifier(Resource):
         args = parser.parse_args()
         email = args['email'].strip()
         password = args['password'].strip()
-        os.environ['email'] = email
-        os.environ['password'] = password
+        # os.environ['email'] = email
+        # os.environ['password'] = password
         client = myfitnesspal.Client(username=email, password=password)
         print('logged as {}'.format(email))
         result = last_day_food_extractor(client)
@@ -119,9 +144,7 @@ class FoodClassifier(Resource):
 api.add_resource(FoodClassifier, '/foodclassifier') 
 
 
-# Only for HEROKU deployment due to daily erasing all filesystem
-url = 'https://raw.githubusercontent.com/atuzhykov/food_classifier/master/MFP_scrapped_food_without_names.csv'
-pipeline(url)
+
 
 if __name__ == '__main__':
     app.run()
